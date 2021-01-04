18e6f859f0b380fa804e21948cfa3703291885e2
tassos96 <tassos.antono96@gmail.com>
2021-01-04 22:54:28 +0200

    fixes

--- reduce.py
+++ reduce.py
@@ -10,9 +10,9 @@ from Project_2.classification import encoder
 
 
 # gpu fix
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# physical_devices = tf.config.experimental.list_physical_devices('GPU')
+# assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+# config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 # get arguments from user
